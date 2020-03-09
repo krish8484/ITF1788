8cf73454ce463af063f62de0aaa574d92baa5f0c
Krish Agarwal <akrish136@gmail.com>
2020-03-09 11:58:15 +0530

    add julia function to generate tests

--- pyfunc.py
+++
@@ -1,4 +0,0 @@
-import runpy
-def gg():
-	runpy.run_module('itf1788', run_name='__main__')
-gg()
--- regenerate_tests.jl
+++ regenerate_tests.jl
@@ -1,5 +1,8 @@
 using PyCall
+# using Conda
+# Conda.add("ply")
+# Conda.add("yaml")
 runpy = pyimport("runpy")
 push!(pyimport("sys")."path", pwd())
 mymodule = pyimport("itf1788")
-runpy.run_module("itf1788")
+runpy.run_module("itf1788", run_name="__main__")
