8cd65aa2d9ce44a30e3e0f069476fe20e5a640ce
Krish Agarwal <akrish136@gmail.com>
2020-03-13 01:40:28 +0530

    Update transfer_tests.jl

--- transfer_tests.jl
+++ transfer_tests.jl
@@ -1,16 +1,16 @@
 using IntervalArithmetic
 using Base.Filesystem
+using Glob
 
 output_file_path = joinpath(dirname(dirname(pathof(IntervalArithmetic))), "test", "ITF1788_tests")
 input_file_path = joinpath(@__DIR__, "output", "julia", "Base.Test", "ValidatedNumerics")
 files = readdir(relpath(input_file_path, @__DIR__))
+files = glob("*.jl", input_file_path)
 
 for file in files
-    if(file=="Makefile")
-        continue
-    end
+    bname = basename(file)
     filepath = joinpath(input_file_path, file)
-    ofilepath = joinpath(output_file_path, file)
+    ofilepath = joinpath(output_file_path, bname)
     cp(relpath(filepath, @__DIR__), ofilepath; force=true)
 end
 
