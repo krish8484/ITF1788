c70549cdbc546e250e1ff825eae3778a0e5abb41
Krish Agarwal <akrish136@gmail.com>
2020-03-14 17:00:24 +0530

    Update transfer_tests.jl

--- transfer_tests.jl
+++ transfer_tests.jl
@@ -3,14 +3,13 @@ using Base.Filesystem
 using Glob
 
 output_file_path = joinpath(dirname(dirname(pathof(IntervalArithmetic))), "test", "ITF1788_tests")
-input_file_path = joinpath(@__DIR__, "output", "julia", "Base.Test", "ValidatedNumerics")
-files = readdir(relpath(input_file_path, @__DIR__))
+input_file_path = joinpath("output", "julia", "Base.Test", "ValidatedNumerics")
 files = glob("*.jl", input_file_path)
 
 for file in files
     bname = basename(file)
-    filepath = joinpath(input_file_path, file)
+    filepath = joinpath(input_file_path, bname)
     ofilepath = joinpath(output_file_path, bname)
-    cp(relpath(filepath, @__DIR__), ofilepath; force=true)
+    cp(filepath, ofilepath; force=true)
 end
 
